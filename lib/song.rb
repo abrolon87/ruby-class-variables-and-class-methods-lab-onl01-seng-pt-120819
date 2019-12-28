2869d7296e9be7933d6c8c2e75806559043c0347
Amanda Rolon <abrolon87@gmail.com>
2019-12-28 14:39:04 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,17 @@
 class Song
-
+  @@count = 0
+  @@artists = []
   attr_accessor :name, :artist, :genre
 
-  def initialize
-    
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists
   end
+
+
+
+
 end
