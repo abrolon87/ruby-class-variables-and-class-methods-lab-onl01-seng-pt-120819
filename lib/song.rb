48b0aaffd5c0947d7eb00059d566bb8811be2c5c
Amanda Rolon <abrolon87@gmail.com>
2019-12-28 21:54:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,17 +1,31 @@
 class Song
   @@count = 0
   @@artists = []
+  @@genres = []
   attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+
+
+    @@artists << artist #unless @@artists.include?(artist)
+    @@genres << genre
     @@count += 1
-    @@artists
+
   end
 
+  def count
+    @@count = count
+  end
 
+  def genres
+    @@genres
+  end
 
+  def artists
+    @@artists
+  end
 
 end
