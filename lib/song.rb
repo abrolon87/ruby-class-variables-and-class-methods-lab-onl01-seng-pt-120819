1517b9c8b76e3b1913fac7097c4f6cfc9447f5a7
Amanda Rolon <abrolon87@gmail.com>
2019-12-29 01:41:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,8 @@ class Song
     @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1
-      songs << :key
-    end
-    songs
+      else
+        genre_count[genre] = 1
+      end
   end
 end
