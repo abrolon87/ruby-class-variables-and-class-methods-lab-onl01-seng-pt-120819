2b0e9e527755f4a586b5b007fe82dfe2819a3abc
Amanda Rolon <abrolon87@gmail.com>
2019-12-29 01:48:05 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,19 +9,13 @@ class Song
     @artist = artist
     @genre = genre
 
-
     @@artists << artist
     @@genres << genre
     @@count += 1
-
   end
 
   def self.count
     @@count
-  end
-
-  def genres
-    @@genres
   end
 
   def self.artists
@@ -32,4 +26,35 @@ class Song
     artists
   end
 
+  def self.genres
+    genres = []    #@@genres.uniq
+    @@genres.each do |genre|
+      genres << genre unless genres.include?(genre)
+    end
+    genres
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
 end
